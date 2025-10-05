232f6ecaac1e33afaa570e1979d611062f64eb8b
mjh-shikder <mjhsikder@gmail.com>
2025-10-05 20:34:29 +0600

    showing custom navlinks option 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import DaisyNav from "./Components/DaisyNav";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <>
+      <header>
+        <NavBar></NavBar>
+        <DaisyNav></DaisyNav>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+const navigationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/home",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services",
+  },
+  {
+    id: 4,
+    name: "Portfolio",
+    path: "/portfolio",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const NavBar = () => {
+  
+
+ 
+
+  return (
+    <nav className="flex justify-between mx-10 ">
+          <h3>My Navbar</h3>
+          <ul className="flex ">
+              {
+                  navigationData.map(route => <li className="mr-9 "><a href={route.path}>{ route.name}</a></li>)
+              }
+          </ul>
+      <button className="btn ">Sing in</button>
+    </nav>
+  );
+};
+
+export default NavBar;
