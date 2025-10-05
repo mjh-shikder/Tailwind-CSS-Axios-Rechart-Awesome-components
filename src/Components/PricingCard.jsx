5c084fbec115c4659b2e7a2a9b5ac86f9f7c25fb
mjh-shikder <mjhsikder@gmail.com>
2025-10-05 22:22:58 +0600

    card pricing button added

--- src/Components/PricingCard.jsx
+++ src/Components/PricingCard.jsx
@@ -5,19 +5,20 @@ const PricingCard = ({ pricing }) => {
     const { name, price, description, features } = pricing;
 
   return (
-    <div className="border bg-amber-600 rounded-2xl p-4 ">
+    <div className=" flex flex-col border bg-amber-600 rounded-2xl p-4 ">
       {/* card header */}
       <div>
         <h1 className="text-7xl">{name}</h1>
         <h1 className="text-3xl">${price}</h1>
           </div>
           {/* card body */}
-          <div className="bg-amber-400 rounded-xl p-3 mt-7">
+          <div className="flex-1 bg-amber-400 rounded-xl p-3 mt-7">
               <p>{description}</p>
               {
                   features.map((feature, index) => <PricingFeature feature={feature} key={index}></PricingFeature>)
               }
           </div>
+          <button className="btn  w-full mt-4">Subscribe</button>
     </div>
   );
 };
--- src/Components/PricingOptions.jsx
+++ src/Components/PricingOptions.jsx
@@ -8,7 +8,7 @@ const PricingOptions = ({ pricingPromise }) => {
   return (
     <div >
       <h2 className="text-5xl ">Get Our Membership</h2>
-          <div className="grid md:grid-cols-3 gap-8">{
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">{
           pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
           }</div>
     </div>
