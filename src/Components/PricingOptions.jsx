06a297d78dd394743f67a051205f484c189c88b4
mjh-shikder <mjhsikder@gmail.com>
2025-10-05 23:03:05 +0600

    also try to add daysi ui cards and modyfiy it

--- src/Components/PricingOptions.jsx
+++ src/Components/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import PricingCard from "./PricingCard";
+import DaysyPricingCard from "./DaysyPricingCard";
 
 const PricingOptions = ({ pricingPromise }) => {
   const pricingData = use(pricingPromise);
@@ -9,7 +10,8 @@ const PricingOptions = ({ pricingPromise }) => {
     <div >
       <h2 className="text-5xl ">Get Our Membership</h2>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">{
-          pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+              //   pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+              pricingData.map(pricing => <DaysyPricingCard key={pricing.id} pricing={pricing}></DaysyPricingCard>)
           }</div>
     </div>
   );
